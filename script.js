8d15e394436515c4bec31719fa3cfe26408c654b
Khelthos <khelthos@hotmail.it>
2020-10-02 17:21:58 +0200

    First edits

---
+++ script.js
@@ -0,0 +1,18 @@
+const videoElem = document.getElementById("video");
+const buttonElem = document.getElementById("button");
+
+// Prompt the user to select a media stream
+async function selectMediaStream() {
+  try {
+    something;
+  } catch (error) {
+    swal.fire({
+      icon: "error",
+      title: "Oops...Something went wrong!",
+      text: error,
+    });
+  }
+}
+
+// OnLoad
+selectMediaStream();
