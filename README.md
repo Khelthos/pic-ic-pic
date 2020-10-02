b18184bd1d68b3bbf1ad8ecf9bfd3958f092aa15
Khelthos <khelthos@hotmail.it>
2020-10-02 17:46:29 +0200

    Project completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Picture-in-Picture Project
+
+[LIVE DEMO](https://khelthos.github.io/pic-ic-pic/)
--- script.js
+++ script.js
@@ -4,7 +4,11 @@ const buttonElem = document.getElementById("button");
 // Prompt the user to select a media stream
 async function selectMediaStream() {
   try {
-    something;
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElem.srcObject = mediaStream;
+    videoElem.onloadedmetadata = () => {
+      videoElem.play();
+    };
   } catch (error) {
     swal.fire({
       icon: "error",
@@ -13,6 +17,15 @@ async function selectMediaStream() {
     });
   }
 }
+
+buttonElem.addEventListener("click", async () => {
+  // Disable button
+  buttonElem.disabled = true;
+  // Start pic-in-pic
+  await videoElem.requestPictureInPicture();
+  // Reset button
+  buttonElem.disabled = false;
+});
 
 // OnLoad
 selectMediaStream();
